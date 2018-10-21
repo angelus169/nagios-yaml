68c59609bb664cbecc06894d258504af4fba19cc
Boris Babiy <boris.babiy@mev.com>
2018-10-21 21:18:05 +0300

    Batman! (this commit has no parents)

---
+++ README.md
@@ -0,0 +1,38 @@
+# Bring YAML to Nagios (r) configuration
+> Convert configuration written in YAML into Nagios objects definition.
+
+## Installation
+
+
+## Usage example
+
+
+## Development setup
+
+
+## Release History
+
+
+* 0.0.1
+    * Initial release
+
+
+## Meta
+
+Borys Babii – [@babiyboris](https://twitter.com/babiyboris) – boris.babiy@mev.com
+
+Distributed under the Apache license 2.0 license. See ``LICENSE`` for more information.
+
+[https://github.com/BorisBabiy](https://github.com/BorisBabiy/)
+
+## Contributing
+
+1. Fork it (<https://github.com/yourname/yourproject/fork>)
+2. Create your feature branch (`git checkout -b feature/fooBar`)
+3. Commit your changes (`git commit -am 'Add some fooBar'`)
+4. Push to the branch (`git push origin feature/fooBar`)
+5. Create a new Pull Request
+
+[travis-image]: https://img.shields.io/travis/BorisBabiy/nagios-yaml/master.svg?style=flat-square
+[travis-url]: https://travis-ci.org/BorisBabiy/nagios-yaml
+[wiki]: https://github.com/BorisBabiy/nagios-yaml/wiki
---
+++ converter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import yaml
+import sys
+import os
+import argparse
+from jinja2 import FileSystemLoader, Environment
+
+
+def main(arguments):
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-i', '--input', help='Input file', default='input/nagios.yml',
+                        type=argparse.FileType('r'))
+    parser.add_argument('-o', '--output', help='Output file', default='output/objects.cfg',
+                        type=argparse.FileType('w'))
+
+    args = parser.parse_args(arguments)
+    yaml_content = yaml.load(args.input)
+    objects_definition = yaml_content['objects']
+    loader = FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates'))
+    environment = Environment(loader=loader, trim_blocks=True, keep_trailing_newline=True)
+    for object_name in objects_definition:
+        objects_template = environment.get_template(object_name + '.j2')
+        print objects_template.render(objects=objects_definition[object_name])
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
